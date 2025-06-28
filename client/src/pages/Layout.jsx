abc82a62d3d2a4954456b30ed5a881e0c543e36c
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2025-06-28 12:22:49 +0530

    Achieved doable way to make RightSidebar scroll independently

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -12,7 +12,7 @@ const Layout = () => {
       <div className="basis-[50%] mr-8 ml-4 border text-white border-x  border-gray-600 pt-4">
         <Outlet />
       </div>
-      <div className="basis-[30%] sticky top-0 self-start pb-8 ">
+      <div className="basis-[30%]  pb-8 h-screen overflow-y-auto sticky top-0 scrollbar-hide">
         <RightSidebar />
       </div>
     </div>
